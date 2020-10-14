d9b96f9c4c693b74156c851f0bae2129f374e596
Daniel Rodrigues <50847349+DanielRodrigues31@users.noreply.github.com>
2020-10-15 00:10:59 +0100

    Queue system and Timer
    Added Queue system for poll entrance aswell as a timer for automatic polls.

--- DemocracyBot/Commands.cs
+++ DemocracyBot/Commands.cs
@@ -8,47 +8,62 @@ using System.Collections.Generic;
 using System.Runtime.CompilerServices;
 using System.Timers;
 using System.Numerics;
+using DSharpPlus.Interactivity;
+using DSharpPlus;
+using Newtonsoft.Json;
+using System.IO;
+using System.Text;
 
 namespace DemocracyBot
 {
-    public class MyCommands
+   public class Commands
     {
         private List<string> DiscordID = new List<string>();
-        
+        public DiscordChannel channel { get; private set; }
 
-        private bool embedstatus;
+        bool QueueOpen { get; set; }
 
+        public async Task Poll()
+        {   
+            var pollembed = new DiscordEmbedBuilder { Title = "Poll", Description = "" };
+            await channel.SendMessageAsync(embed: pollembed);   
+        }
 
+      
         [Command("Enter")]
         public async Task Enter (CommandContext ctx) 
         {
             var roles = ctx.Member.Roles.ToString();
             var user = ctx.Member.Username;
 
-            for (int i = 0; i < roles.Length; i++) 
+            if (roles == "everyone" && QueueOpen == true) //verification 
             {
-                if (roles == "" && embedstatus == true) 
-                {
-                    DiscordID.Add(user);
-                }
+                DiscordID.Add(user);
             }
+            
 
             
         }
 
-        [Command("poll")]
-        public async Task Poll (CommandContext ctx) 
+        public async Task Timer() 
         {
-            
-            var pollembed = new DiscordEmbedBuilder { Title = "Poll", Description = "Modpoll"};
-            await ctx.Channel.SendMessageAsync(embed: pollembed);
-            
+            //reads json
+            var json = string.Empty;
+            using (var Read = File.OpenRead("TokenFile.json"))
+            using (var Sort = new StreamReader(Read, new UTF8Encoding(false)))
+                json = await Sort.ReadToEndAsync();
+            var tokenfile = JsonConvert.DeserializeObject<TokenFile>(json);
 
-            //timer create embed, expirery timer, vector for user id's
+            //timer
+            TimeSpan endtime = TimeSpan.FromDays(tokenfile.Duration);
+            endtime = endtime.Subtract(TimeSpan.FromSeconds(1));
+
+            
+            if (endtime == TimeSpan.FromSeconds(0))
+            {
+                await Poll();
+                QueueOpen = true;
+            }
         }
-
-        
-
-        
     }
 }
